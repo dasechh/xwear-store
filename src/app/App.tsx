9e71caa47a49e240260370719d384f77c08c5ee3
dasechh <kosarev-2004@mail.ru>
2025-10-30 22:44:10 +0300

    added muiltiple routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,13 +6,18 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<HomePage />} />
         <Route path="/search" element={<HomePage />} />
         <Route path="/calculator" element={<HomePage />} />
         <Route path="/favourite" element={<HomePage />} />
         <Route path="/profile" element={<HomePage />} />
         <Route path="/cart" element={<HomePage />} />
         <Route path="/catalog/*" element={<HomePage />} />
+        <Route path="/shipping" element={<HomePage />} />
+        <Route path="/payment" element={<HomePage />} />
+        <Route path="/blog" element={<HomePage />} />
+        <Route path="/contact" element={<HomePage />} />
+        <Route path="/privacy" element={<HomePage />} />
+        <Route path="/terms" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
